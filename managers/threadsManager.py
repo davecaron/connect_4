a45e733c6ce0b01014e70d55d2cf5317a38d721a
Charles Vandal <charlesvandal@gmail.com>
2022-01-30 22:22:46 -0500

    Made threads member private

--- managers/threadsManager.py
+++ managers/threadsManager.py
@@ -1,25 +1,26 @@
 from gameManager import GameManager
 
+
 class ThreadsManager:
 
     def __init__(self):
-        self.threads = []
+        self._threads = []
 
     def createThreads(self):
         print("Creating thread %s" % GameManager.__name__)
         thread = GameManager()
         thread.daemon = True
         print("Thread created")
-        self.threads.append(thread)
+        self._threads.append(thread)
 
     def startThreads(self):
-        for thread in self.threads:
-            if (thread != None):
+        for thread in self._threads:
+            if thread is not None:
                 thread.start()
 
     def stopThreads(self):
-        for thread in self.threads:
-            if (thread != None):
+        for thread in self._threads:
+            if thread is not None:
                 thread.stop()
                 thread.join()
 
