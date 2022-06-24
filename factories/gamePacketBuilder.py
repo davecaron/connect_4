2da759902ee2b308e7f4d9e12527a948cbe8c7d5
Charles Vandal <charlesvandal@gmail.com>
2022-06-24 19:47:09 -0400

    Renamed GamePacketFactory  to gamePacketBuilder

--- factories/gamePacketBuilder.py
+++ factories/gamePacketBuilder.py
@@ -4,10 +4,10 @@ from defines.packetDefines import GamePacket
 from defines.gameDefines import PlayerId
 
 
-class GamePacketFactory:
+class GamePacketBuilder:
 
     @staticmethod
     def getPacket(command: GameCommands = None, gameData: GameData = None, playedColumn: int = -1, playedRow: int = -1,
-                  gameFinished=False, winningPlayer: PlayerId = None) -> GamePacket:
+                  gameFinished=False, currentPlayerId: PlayerId = PlayerId.NO_PLAYER, winningPlayerId: PlayerId = PlayerId.NO_PLAYER) -> GamePacket:
         return GamePacket(command=command, gameData=gameData, playedColumn=playedColumn, playedRow=playedRow,
-                          gameFinished=gameFinished, winningPlayer=winningPlayer)
+                          currentPlayerId=currentPlayerId, gameFinished=gameFinished, winningPlayerId=winningPlayerId)
