b70595591251827cd5e0e91cab96ca3f08152f8e
Charles Vandal <charlesvandal@gmail.com>
2022-06-24 19:48:15 -0400

    Added uiDefines

---
+++ defines/uiDefines.py
@@ -0,0 +1,18 @@
+from PyQt6.QtGui import QColor
+
+
+class CellDefines:
+    CELL_WIDTH_PX = 100
+    CELL_HEIGHT_PX = 100
+    BACKGROUND_COLOR = QColor(0, 162, 232)
+
+
+class TokenDefines:
+    EMPTY_COLOR = QColor(255, 255, 255)
+    PLAYER1_COLOR = QColor(0, 128, 255)
+    PLAYER2_COLOR = QColor(255, 53, 53)
+
+
+class BoardDefines:
+    X_MARGINS_PX = 10
+    Y_MARGINS_PX = 10
